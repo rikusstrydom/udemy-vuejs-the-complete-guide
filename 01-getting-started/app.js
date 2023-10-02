4e7d312b5816bd08f067be5218de5c3b22686f46
Rikus Strydom <rikusstrydom@gmail.com>
2023-10-02 21:44:53 +0200

    Getting Started

---
+++ 01-getting-started/app.js
@@ -0,0 +1,29 @@
+Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            enteredValue: ''
+        };
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue);
+            this.enteredValue = '';
+        }
+    }
+}).mount('#app');
+
+// const buttonElement = document.querySelector('button');
+// const inputElement = document.querySelector('input');
+// const listElement = document.querySelector('ul');
+//
+// function addGoal() {
+//     const enteredValue = inputElement.value;
+//     const listItemElement = document.createElement('li');
+//     listItemElement.textContent = enteredValue;
+//     listElement.appendChild(listItemElement)
+//
+//     inputElement.value = '';
+// }
+//
+// buttonElement.addEventListener('click', addGoal)
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# vuejs-the-complete-guide
+# Vue - The Complete Guide
+
+- [Link to Course](https://www.udemy.com/course/vuejs-2-the-complete-guide/)
+- [Certificate of Completion](https://www.udemy.com/certificate/UC-c4322d6b-aabc-4dc0-b12e-cd24846ee378)
+
+![Certificate of Completion](https://github.com/rikusstrydom/udemy-understanding-typescript/blob/main/Certificate.jpg?raw=true)
