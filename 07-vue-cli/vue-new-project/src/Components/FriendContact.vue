a211fe8c9cdccdedf254966ed64114e9c5406bb9
Rikus Strydom <rikusstrydom@gmail.com>
2023-10-10 08:37:49 +0200

    Vue CLI

---
+++ 07-vue-cli/vue-new-project/src/Components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">{{ showDetails ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="showDetails">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+
+export default {
+  name: 'friend-contact',
+  props: {
+    friend: {}
+  },
+  data() {
+    return {
+      showDetails: false
+    }
+  },
+  methods: {
+    toggleDetails() {
+      this.showDetails = !this.showDetails;
+    }
+  }
+}
+
+</script>
