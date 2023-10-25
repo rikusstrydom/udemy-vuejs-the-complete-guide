81cf1c855c688a282ba8dc8a6dce56d9aef3cf83
Rikus Strydom <rikusstrydom@gmail.com>
2023-10-25 21:56:56 +0200

    Diving deeper into components

---
+++ 09-diving-deeper-into-components/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <!--    <badge-list></badge-list>-->
+    <!--    <user-info-->
+    <!--        :full-name="activeUser.name"-->
+    <!--        :info-text="activeUser.description"-->
+    <!--        :role="activeUser.role"-->
+    <!--    ></user-info>-->
+    <!--    <course-goals>-->
+    <!--      <template #default="slotProps">-->
+    <!--        <h2>{{ slotProps.item }}</h2>-->
+    <!--        <p>{{ slotProps.anotherProp }}</p>-->
+    <!--      </template>-->
+    <!--    </course-goals>-->
+
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!--    <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>-->
+    <!--    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals>-->
+
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
+
+  </div>
+</template>
+
+<script>
+
+
+import TheHeader from "./components/layout/TheHeader.vue";
+import ActiveGoals from "@/components/ActiveGoals.vue";
+import ManageGoals from "@/components/ManageGoals.vue";
+
+export default {
+  components: {TheHeader, ActiveGoals, ManageGoals},
+  // components: {TheHeader, BadgeList, UserInfo, CourseGoals},
+  data() {
+    return {
+      selectedComponent: 'active-goals',
+      activeUser: {
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    };
+  },
+  methods: {
+    setSelectedComponent(comp) {
+      this.selectedComponent = comp;
+    }
+  }
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ 09-diving-deeper-into-components/src/components/UserInfo.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <base-card>
+      <template #header>
+        <h3>{{ fullName }}</h3>
+        <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+      </template>
+      <template #default>
+        <p>{{ infoText }}</p>
+      </template>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import BaseCard from "@/components/UI/BaseCard.vue";
+import BaseBadge from "@/components/UI/BaseBadge.vue";
+
+export default {
+  components: {BaseBadge, BaseCard},
+  props: ['fullName', 'infoText', 'role'],
+};
+</script>
