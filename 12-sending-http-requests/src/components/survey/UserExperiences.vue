b2f27d1b4a93a7caef3560eebc12898b79be9581
Rikus Strydom <rikusstrydom@gmail.com>
2023-11-05 17:32:33 +0200

    Sending Http Requests

---
+++ 12-sending-http-requests/src/components/survey/UserExperiences.vue
@@ -0,0 +1,70 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Submitted Experiences</h2>
+      <div>
+        <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
+      </div>
+      <p v-if="isLoading">Loading...</p>
+      <p v-else-if="!isLoading && error">{{ error }}</p>
+      <p v-else-if="!isLoading && (!results || results.length === 0)">No stored experiences found...</p>
+      <ul v-else>
+        <survey-result
+            v-for="result in results"
+            :key="result.id"
+            :name="result.name"
+            :rating="result.rating"
+        ></survey-result>
+      </ul>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import SurveyResult from './SurveyResult.vue';
+
+export default {
+  data() {
+    return {
+      results: [],
+      isLoading: false,
+      error: null
+    }
+  },
+  components: {
+    SurveyResult,
+  },
+  methods: {
+    loadExperiences() {
+      this.isLoading = true;
+      this.error = null;
+      fetch('https://udemy-vuejs-17713-default-rtdb.europe-west1.firebasedatabase.app/surveys.json', {}).then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+      }).then(data => {
+        this.isLoading = false;
+        const results = [];
+        for (const key in data) {
+          results.push({id: key, ...data[key]})
+        }
+        this.results = results;
+      }).catch(error => {
+        this.isLoading = false;
+        this.error = 'Failed to fetch data - please try again later!'
+      });
+    }
+  },
+  mounted() {
+    this.loadExperiences();
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
